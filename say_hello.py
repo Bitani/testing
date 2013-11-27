2157c39e3bec0a8503398544ae348872f4525cec
Zach McGohan <zmcgohan@gmail.com>
2013-11-26 23:56:08 -0500

    Added another line.

--- say_hello.py
+++ say_hello.py
@@ -2,6 +2,7 @@ def sayHello(message):
 	"""This function just says hello."""
 	print message + " My name's Zach, by the way." # and here's a comment
 	print "And here's something different..."
+	print "And something else."
 
 if __name__ == "__main__":
 	sayHello("Hello. How are you?");
