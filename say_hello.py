9349180643ef6972bb06bc685883196109be84e2
Zach McGohan <zmcgohan@gmail.com>
2013-11-28 15:57:02 -0500

    Test commit #1...

--- say_hello.py
+++ say_hello.py
@@ -1,9 +1,6 @@
 def sayHello(message):
 	"""This function just says hello."""
 	print message + " My name's Zach, by the way." # and here's a comment
-	print "And here's something different..."
-	print "And something else."
-	print "Nice to meet you!"
 
 if __name__ == "__main__":
 	sayHello("Hello. How are you?");
